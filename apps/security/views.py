f4835debda5c8ae74cc62c78b01ede001405a590
Yonathan Aviles <yonathanaviles123@gmail.com>
2021-08-11 17:38:22 -0400

    Update views.py

--- apps/security/views.py
+++ apps/security/views.py
@@ -98,7 +98,7 @@ class UserViewSet(ModelViewSet):
     filter_backends = [DjangoFilterBackend, SearchFilter]
     filterset_class = UserFilter
     serializer_class = UserSimpleSerializer
-    search_fields = ['name', 'last_name' 'email']
+    search_fields = ['name', 'last_name' 'email', 'code']
     permission_classes = (AllowAny,)
 
     def paginate_queryset(self, queryset):
