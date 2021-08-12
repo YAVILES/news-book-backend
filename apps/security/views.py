ac7cf21ab1fbef20191f6f20c41327d1cf20be5a
Yonathan Aviles <yonathanaviles123@gmail.com>
2021-08-11 20:53:05 -0400

    Update views.py

--- apps/security/views.py
+++ apps/security/views.py
@@ -221,7 +221,8 @@ class RoleViewSet(ModelViewSet):
     serializer_class = RoleDefaultSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter]
     search_fields = ['first_name', 'last_name']
-
+    permission_classes = (AllowAny,)
+    
     def paginate_queryset(self, queryset):
         """
         Return a single page of results, or `None` if pagination is disabled.
