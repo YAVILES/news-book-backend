3c889f6e6ecb23e042fb5037c117cc95dbc2e6b9
Yonathan Aviles <yonathanaviles123@gmail.com>
2021-08-19 12:05:26 -0400

    Update views.py

--- apps/main/views.py
+++ apps/main/views.py
@@ -346,7 +346,7 @@ class NewsViewSet(ModelViewSet):
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
-        send_email.delay(TypeNews.objects.get(id=serializer.data['type_news']).description, '', [user.email])
+        #send_email.delay(TypeNews.objects.get(id=serializer.data['type_news']).description, '', [user.email])
 
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
