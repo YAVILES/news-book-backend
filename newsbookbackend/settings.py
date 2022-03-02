ad2f1c2bcc82c0997b60e69b443c8a62291c6778
YAVILES <yonathanaviles123@gmail.com>
2022-03-02 11:00:07 -0400

    Update settings.py

--- newsbookbackend/settings.py
+++ newsbookbackend/settings.py
@@ -49,7 +49,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['194.163.161.64']
 
 # Application definition
 
