2e4c3b482a25597fd9e4fcef9aecd947782413b6
YAVILES <yonathanaviles123@gmail.com>
2022-03-02 12:03:10 -0400

    Update settings.py

--- newsbookbackend/settings.py
+++ newsbookbackend/settings.py
@@ -235,7 +235,7 @@ REST_FRAMEWORK = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/var/www/static/'
 STATIC_ROOT = '/var/www/static'  # os.path.join(BASE_DIR, "static")
 
 STATICFILES_FINDERS = [
