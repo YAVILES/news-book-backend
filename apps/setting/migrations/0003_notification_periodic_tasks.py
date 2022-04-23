575d97c7d9ac30d4c3431191490dbb61ed8b79de
YAVILES <yonathanaviles123@gmail.com>
2022-04-23 16:03:43 -0400

    Update 0003_notification_periodic_tasks.py

--- apps/setting/migrations/0003_notification_periodic_tasks.py
+++ apps/setting/migrations/0003_notification_periodic_tasks.py
@@ -6,7 +6,6 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('django_celery_beat', '0017_auto_20220422_1155'),
         ('setting', '0002_auto_20220328_1604'),
     ]
 
