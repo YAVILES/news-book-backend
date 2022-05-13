d1e30d35cf1d16554d6299b9b59e2a925a1ee406
YAVILES <yonathanaviles123@gmail.com>
2022-05-13 14:37:12 -0400

    Update views.py

--- apps/customers/views.py
+++ apps/customers/views.py
@@ -1,12 +1,8 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import viewsets, status
 from rest_framework.filters import SearchFilter
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
+from rest_framework.viewsets import ModelViewSet
 
 from .models import Client, Domain
 from .serializers import ClientSerializer, DomainSerializer
@@ -16,7 +12,7 @@ class ClientViewSet(ModelViewSet):
     serializer_class = ClientSerializer
     queryset = Client.objects.all()
     filter_backends = [DjangoFilterBackend, SearchFilter]
-    search_fields = ['name', 'domain_url']
+    search_fields = ['name', 'email']
     permission_classes = (AllowAny,)
     authentication_classes = []
 
